382516e12166715ac4643f96f8b6e7082a342b74
karini925 <karin.isaev@gmail.com>
2020-05-12 18:53:12 -0400

    started first tidy-tuesday challenge

---
+++ 05122020_tidytuesday.R
@@ -0,0 +1,23 @@
+#tidy-tuesday
+#date: May 12, 2020
+#author: karin isaev
+
+#this week we are looking at volcano eruption data
+
+#[1] load packages first
+
+
+
+#[2] load some data
+
+
+
+#[3] prepare data for plotting
+
+
+
+#[4] plot data
+
+
+
+#[5] save plot 
